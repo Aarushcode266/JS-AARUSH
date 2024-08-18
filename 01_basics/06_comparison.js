6b647af5bfb54421f4a85d392686cace7345903e
Aarushcode266 <155549595+Aarushcode266@users.noreply.github.com>
2024-08-18 06:55:23 +0000

    comparison done

---
+++ 01_basics/06_comparison.js
@@ -0,0 +1,24 @@
+console.log(null> 0);
+console.log(null == 0);
+console.log(null>= 0);
+
+
+// comparison operation >= ,<=  < , > works differently as compared to == 
+// comparison operaton makes null to 0 and then accordind to that result is produced
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+
+
+// above written statements should be avoided 
+
+// "===" strict comparison
+
+console.log("2" === "2");
+
+
+
+
