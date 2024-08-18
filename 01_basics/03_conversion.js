65f02e95ee0d8859eca2e598bda523e37e4aed6c
Aarushcode266 <155549595+Aarushcode266@users.noreply.github.com>
2024-08-18 06:02:54 +0000

    conersion and variables is done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,71 @@
+let score = 43;
+console.log(typeof score);
+
+console.log(typeof 43);
+
+
+console.log(typeof (score));
+
+let marks = "54";
+
+console.log(typeof marks);
+console.log(typeof "54");
+
+let newvalue = Number(marks);
+console.log(typeof newvalue);
+
+console.log(newvalue);
+
+
+
+
+
+let word = null;
+
+
+console.log(typeof word);
+let val = Number(word);
+console.log(typeof val);
+console.log(val);
+
+
+ let syntax = undefined;
+
+
+ console.log(typeof syntax);
+ let ano = Number(syntax);
+ console.log(ano);
+ console.log(typeof ano);
+
+
+ let ky = "aarush";
+ let sc = Number(ky);
+ console.log(sc);
+
+ console.log(typeof sc);
+
+
+
+
+ let isLogged = 1;
+ let check = Boolean(isLogged);
+
+ console.log(check);
+
+ console.log(typeof check);
+
+// "" => means false 
+// 1 in boolean return true;
+// 0 return false in boolean
+// "string" => anything written in these brackets return true
+
+
+let map = 20;
+let strnum = String(map);
+console.log(typeof strnum);
+console.log(strnum);
+
+
+
+
+
