64e68c6d63c9e018472341fd428f4d89461bbecd
Aarushcode266 <155549595+Aarushcode266@users.noreply.github.com>
2024-08-18 06:23:50 +0000

    conversion done

---
+++ 01_basics/04_conversionoperations.js
@@ -0,0 +1,40 @@
+let value = 55;
+let newvalue = -value;
+console.log(newvalue);
+
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/3);
+console.log(2%3);
+console.log(2**2);
+
+
+let str1 = "aarush";
+let str2 = "pandey";
+let str3 = str1 +str2;
+console.log(str3);
+
+
+
+console.log("1" + 2);
+console.log("2" + 3 + 3);
+console.log(1 + 3 + "4");
+console.log("1" + "5" + 6);
+
+
+console.log(true);
+console.log(+true);
+
+
+let gamescore = 100;
+gamescore++;
+console.log(gamescore);
+
+let anotherscore = 600;
+++anotherscore;
+console.log(anotherscore);
+
+// read prefix and postfix operators
