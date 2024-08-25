187f6f87ad678d561768c7b3c9acc1672182c87c
Aarushcode266 <155549595+Aarushcode266@users.noreply.github.com>
2024-08-25 04:21:46 +0000

    scope done

---
+++ 03_basics/04_scope.js
@@ -0,0 +1,87 @@
+
+
+function one() {
+    const user = "aarush";
+
+    function two() {
+        console.log(`my name si ${user}`);
+
+        const website = "youtube";
+
+        console.log(website)
+    }
+
+    two();
+}
+one();
+
+
+if(true) {
+    const username = "aarush";
+
+    if(username === "aarush") {
+     const website = "codespacxe";
+        console.log(username + website);
+
+
+
+    }
+
+   //  console.log(website);
+}
+
+// console.log(username);
+
+function user1(num) {
+
+
+
+    return num +2;
+
+
+}
+
+
+user1(5);
+
+
+
+
+
+
+const x = function(num) {
+    return num + 5;
+
+}
+
+x();
+
+
+
+
+
+
+console.log(user1(5));   // this can be done it is possible
+function user1(num) {
+
+
+
+    return num +2;
+
+
+}
+
+
+
+
+
+
+
+/*    x(6);
+const x = function(num) { // this is not correct way of decleartion
+    return num + 5;
+
+}
+*/
+
+
